957e579bf092263b4b06f128a9c7ff796a66645a
Hewlette Dibble <hcodydibble@gmail.com>
2017-08-15 14:04:45 -0700

    put all my method calls into a for loop

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 'use strict';
 
-var timeOfDay = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: ', 'Total: '];
-
+var timeOfDay = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: '];
+var storesOwned = [];
 function Store(name,minCust,maxCust,avgCookie){
   this.name = name;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.aveCookiePerCust = avgCookie;
   this.cookiesPerHour = [];
+  this.totalSales = 0;
   this.custPerHour = function(){
     return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
   };
   this.salesPerHour = function(){
-    var totalSales = 0;
+    this.totalSales = 0;
     for(var i = 0; i < 14; i++){
       var foo = this.custPerHour() * Math.floor(this.aveCookiePerCust);
-      totalSales = (totalSales + foo);
+      this.totalSales += foo;
       this.cookiesPerHour.push(foo);
     }
-    this.cookiesPerHour.push(totalSales);
   };
   this.goToPage = function(){
     var salesSection = document.getElementById('sales_section');
@@ -33,25 +33,20 @@ function Store(name,minCust,maxCust,avgCookie){
       newLi.innerText = pikeTime;
       salesList.appendChild(newLi);
     }
+    var listTotal = document.createElement('li');
+    listTotal.innerText = 'Total: ' + this.totalSales + ' cookies.';
+    salesList.appendChild(listTotal);
   };
+  storesOwned.push(this);
 };
 
 var firstAndPike = new Store('1st and Pike',23,65,6.3);
-firstAndPike.salesPerHour();
-firstAndPike.goToPage();
-
 var seaTacAirport = new Store('SeaTac Airport',3,24,1.2);
-seaTacAirport.salesPerHour();
-seaTacAirport.goToPage();
-
 var seattleCenter = new Store('Seattle Center',11,38,3.7);
-seattleCenter.salesPerHour();
-seattleCenter.goToPage();
-
 var capitolHill = new Store('Capitol Hill',20,38,2.3);
-capitolHill.salesPerHour();
-capitolHill.goToPage();
-
 var alkiStore = new Store('Alki',2,16,4.6);
-alkiStore.salesPerHour();
-alkiStore.goToPage();
+
+for(var i = 0; i < storesOwned.length; i++){
+  storesOwned[i].salesPerHour();
+  storesOwned[i].goToPage();
+}
