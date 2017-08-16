8cb634aaf712fbc6feef9c57dcd931a908fe1bb5
Hewlette Dibble <hcodydibble@gmail.com>
2017-08-15 21:33:52 -0700

    TAKE THAT YOU DAMN TABLES! got it all working finally

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 var timeOfDay = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 var storesOwned = [];
+var hourlyTotals = [];
 
 function Store(name,minCust,maxCust,avgCookie){
   this.name = name;
@@ -45,14 +46,15 @@ var seattleCenter = new Store('Seattle Center',11,38,3.7);
 var capitolHill = new Store('Capitol Hill',20,38,2.3);
 var alkiStore = new Store('Alki',2,16,4.6);
 
-var createTable = function(){
-  var tableLocation = document.getElementById('sales_section');
-  var tableBody = document.createElement('table');
-  tableLocation.appendChild(tableBody);
-  tableBody.id = 'tableHolder';
-  var blankSpace = document.createElement('th');
-  blankSpace.innerText = ' ';
-  tableBody.appendChild(blankSpace);
+var tableLocation = document.getElementById('sales_section');
+var tableBody = document.createElement('table');
+tableLocation.appendChild(tableBody);
+tableBody.id = 'tableHolder';
+var blankSpace = document.createElement('th');
+blankSpace.innerText = ' ';
+tableBody.appendChild(blankSpace);
+
+var createdHeader = function(){
   for (var i = 0; i < timeOfDay.length; i++) {
     var timeRow = document.createElement('th');
     timeRow.innerText = timeOfDay[i];
@@ -62,9 +64,31 @@ var createTable = function(){
   totalEnd.innerText = 'Daily Location Totals';
   tableBody.appendChild(totalEnd);
 };
-createTable();
+createdHeader();
 
 for(var i = 0; i < storesOwned.length; i++){
   storesOwned[i].salesPerHour();
   storesOwned[i].goToPage();
 }
+
+for (var i = 0; i < timeOfDay.length; i++) {
+  var hourTotals = storesOwned[0].cookiesPerHour[i] + storesOwned[1].cookiesPerHour[i] + storesOwned[2].cookiesPerHour[i] + storesOwned[3].cookiesPerHour[i] + storesOwned[4].cookiesPerHour[i];
+  hourlyTotals.push(hourTotals);
+}
+
+var createFooter = function(){
+  var totalsHope = document.getElementById('tableHolder');
+  var totalsRow = document.createElement('tr');
+  totalsRow.innerText = 'Totals';
+  totalsHope.appendChild(totalsRow);
+  for(var i = 0; i < timeOfDay.length; i++){
+    var newTd = document.createElement('td');
+    var shopSales = hourlyTotals[i];
+    newTd.innerText = shopSales;
+    totalsRow.appendChild(newTd);
+  }
+  var totalSalesTotal = document.createElement('td');
+  totalSalesTotal.innerText = storesOwned[0].totalSales + storesOwned[1].totalSales + storesOwned[2].totalSales + storesOwned[3].totalSales + storesOwned[4].totalSales;
+  totalsRow.appendChild(totalSalesTotal);
+};
+createFooter();
